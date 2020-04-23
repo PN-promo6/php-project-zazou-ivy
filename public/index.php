aca67c34ff2ccd7233a712bdc52871033f79b86b
lea zaoui <lea.myriam.zaoui@gmail.com>
2020-04-23 11:17:47 +0200

    new version

---
+++ public/index.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title> Kaîko-bu! 漫画</title>
+    <link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a id="logo" class="navbar-brand" href="#">Kaîko-bu! 漫画</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sign in</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <header id="image">
+
+        <h1>Retrouvez vos meilleurs Openings d'anime !</h1>
+
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="card col-lg-4 mb-2">
+                <img src="images/siniki.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> Shingeki no Kyojin</h5>
+                    <p class="card-text">Linked Horizon - Shinzou wo Sasageyo!</p>
+                    <p class="card-text">Meilleur Opening ever !</p>
+
+                    <a href="#" class="btn btn-primary">Add</a>
+                </div>
+            </div>
+            <div class="card  col-lg-4 mb-2">
+                <img src="images/ergo.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Ergo proxy</h5>
+                    <p class="card-text">Monoral - Kiri</p>
+                    <p class="card-text">Bababa ça claque !</p>
+
+                    <a href="#" class="btn btn-primary">Add</a>
+                </div>
+            </div>
+            <div class="card col-lg-4 mb-2">
+                <img src="images/bleach.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Bleach</h5>
+                    <p class="card-text">Orange range - Asterik</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card col-lg-4 mb-2">
+                <img src="images/code.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Code geass</h5>
+                    <p class="card-text">Flow - Colors</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card col-lg-4 mb-2">
+                <img src="images/Erased1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Erased</h5>
+                    <p class="card-text">Re:Re - Boku dake ga inai machi</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card col-lg-4 mb-2">
+                <img src="images/gto.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> GTO</h5>
+                    <p class="card-text">porno Graffiti - Hitori no yoru</p>
+                    <a href="#" class="btn btn-primary">Add</a>
+                </div>
+            </div>
+            <div class="card col-lg-4 mb-2">
+                <img src="images/siniki.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> Shingeki no Kyojin</h5>
+                    <p class="card-text">Linked Horizon - Shinzou wo Sasageyo!</p>
+                    <a href="#" class="btn btn-primary">Add</a>
+                </div>
+            </div>
+            <div class="card col-lg-4 mb-2">
+                <img src="images/siniki.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> Shingeki no Kyojin</h5>
+                    <p class="card-text">Linked Horizon - Shinzou wo Sasageyo!</p>
+                    <a href="#" class="btn btn-primary">Add</a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
